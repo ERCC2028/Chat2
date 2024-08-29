d69f52a581a64640cf0b8bef6520ae69abd5ff05
ERCC2028 <eloy.requejo.caillot@gmail.com>
2024-08-29 15:26:07 +0200

    Bug fixed : month + 1

--- static/script.js
+++ static/script.js
@@ -277,7 +277,7 @@ function embedError(message) {
  */
 function formatDate(timestamp) {
     const date = new Date(timestamp);
-    return `${date.getDate().toString().padStart(2, "0")}/${date.getMonth().toString().padStart(2, "0")} ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
+    return `${date.getDate().toString().padStart(2, "0")}/${(date.getMonth() + 1).toString().padStart(2, "0")} ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
 }
 
 /**
