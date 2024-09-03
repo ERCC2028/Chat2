b7720c6266209a2cdb5513fa6e5b5858f726aa4c
ERCC2028 <106447564+ERCC2028@users.noreply.github.com>
2024-09-03 20:46:58 +0200

    Update index.js 750

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ function parseMessage(data) {
  * @param {string} where
  * @returns {Promise<string>}
  */
-function stringifyMessagesFromSQL(where = "WHERE id > 566") {
+function stringifyMessagesFromSQL(where = "WHERE id > 750") {
     return new Promise((resolve, reject) => {
         database.query(`SELECT * FROM Messages ${where};`, (err, results) => {
             if (err) {
